cc30b365d7ec03892befa9bda5814f8c4fbbc838
Anjali-Nayak-prog <anjaliknayak143@gmail.com>
2026-09-22 11:34:34 +0530

    Add Paradise Nursery application

---
+++ src/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant shop dedicated to bringing
+        beautiful and healthy plants to your home.
+      </p>
+
+      <p>
+        We offer a wide variety of indoor and outdoor plants, carefully
+        selected for plant lovers of all experience levels.
+      </p>
+
+      <p>
+        Our mission is to make it easy for everyone to create a greener,
+        healthier, and more beautiful living space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
